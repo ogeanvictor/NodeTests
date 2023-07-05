ffbd09593543931e317558eb328d8730dcd538bc
Gean Victor <geanvictorduque@gmail.com>
2023-07-05 18:23:56 -0300

    fix: Fix integration test

--- test/user.test.js
+++ test/user.test.js
@@ -1,6 +1,10 @@
 require('dotenv/config');
 
 const mongoose = require('mongoose');
+const request = require('supertest');
+
+const app = require('../src/app')
+
 const { save } = require('../src/services/user');
 
 describe('Test user create route', () => {
@@ -12,27 +16,37 @@ describe('Test user create route', () => {
     it('should be create user', async () => {
         const savedUser = await save({
             name: 'Name test',
-            username: 'usernameTest2',
-            email: 'email2@test.com'
+            username: 'usernameTest26',
+            email: 'email23@test.com'
         });
         
         expect(savedUser).toHaveProperty('_id');
     });
 
+    it('should be able to create user (test integration)', async () => {
+        const response = request(app).post('/user/').send({
+            name: 'Name supertest',
+            username: 'usernameSupertest1',
+            email: 'email1@supertest.com'
+        })
+
+        expect((await response).statusCode).toBe(200);
+    });
+
     it('should not be able to create user with username already exists', async () => {
         const userOne = await save({
             name: 'Name test one',
-            username: 'usernameTestDuplicated',
-            email: 'emailduplicated@test.com'
+            username: 'usernameTestDuplicated2',
+            email: 'emailduplicated1@test.com'
         });
 
         const userTwo = await save({
             name: 'Name test two',
             username: 'usernameTestDuplicated',
-            email: 'emailduplicated@test.com'
+            email: 'emailduplicated1@test.com'
         });
 
-        expect(userTwo).toHaveProperty('message')
+        expect(userTwo).toHaveProperty('message');
     });
 
     afterAll(async () => {
