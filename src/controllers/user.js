9622550b68c55850364ac5c9f5513c7b3f1f8c85
Gean Victor <geanvictorduque@gmail.com>
2023-07-05 18:33:25 -0300

    feat: Add crud controller

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -11,4 +11,40 @@ exports.save = async (req,res,next) => {
     } catch (error) {
         res.status(HttpStatusCodes.StatusCodes.BAD_REQUEST).send({"message": error.message});
     }
+};
+
+exports.get = async (req, res) => {
+    try {
+        let users = await service.get(req.body);
+        res.status(HttpStatusCodes.StatusCodes.OK).send(users);
+    } catch (error) {
+        res.status(HttpStatusCodes.StatusCodes.BAD_REQUEST).send({"message": error.message});
+    }
+};
+
+exports.getById = async (req, res) => {
+    try {
+        let user = await service.getById(req.params);
+        res.status(HttpStatusCodes.StatusCodes.OK).send(user);
+    } catch (error) {
+        res.status(HttpStatusCodes.StatusCodes.BAD_REQUEST).send({"message": error.message});
+    }
 }
+
+exports.update = async (req,res) => {
+    try {
+        let user = await service.update(req.body);
+        res.status(HttpStatusCodes.StatusCodes.OK).send(user);
+    } catch (error) {
+        res.status(HttpStatusCodes.StatusCodes.BAD_REQUEST).send({"message": error.message});
+    }
+};
+
+exports.delete = async (req, res) => {
+    try {
+        let user = await service.delete(req.params);
+        res.status(HttpStatusCodes.StatusCodes.OK).send(user);
+    } catch (error) {
+        res.status(HttpStatusCodes.StatusCodes.BAD_REQUEST).send({"message": error.message});
+    }
+};
