ed4e6fcb30ea08ed555110a1a9fb3c77420eac8a
Gean Victor <geanvictorduque@gmail.com>
2023-07-05 18:32:41 -0300

    feat: Add crud routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -5,6 +5,10 @@ const router = express.Router();
 
 const controller = require('../controllers/user')
 
-router.post('/', controller.save)
+router.post('/', controller.save);
+router.get('/', controller.get);
+router.get('/:id', controller.getById);
+router.put('/', controller.update);
+router.delete('/:id', controller.delete);
 
 module.exports = router;
